c490fcd315489dd7fac3743d8b9e6596b1826067
Frank McIngvale <fmcingvale@gmail.com>
2022-02-01 19:09:44 -0600

    updated c++ regression script. all tests passed with no differences (as expected)

--- c++/regression.sh
+++ c++/regression.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # run this before checking in revisions so i can see if the unittest results
 # changed from the expected values
-./repl.exe -test unittest_core.txt > expect_unittest_core.txt
-./repl.exe -test unittest_basic.txt > expect_unittest_basic.txt
-./repl.exe -test unittest_errors.txt > expect_unittest_errors.txt
+./repl.exe -test ../unittests/unittest_core.txt > ../unittests/RESULTS/c++/expect_unittest_core.txt
+./repl.exe -test ../unittests/unittest_basic.txt > ../unittests/RESULTS/c++/expect_unittest_basic.txt
+./repl.exe -test ../unittests/unittest_errors.txt > ../unittests/RESULTS/c++/expect_unittest_errors.txt
 # run c++ unittests too
 ./test.exe
