d80a692db634e1984195757b12dc8ded9eef008a
Frank McIngvale <fmcingvale@gmail.com>
2022-03-24 18:53:03 -0500

    count-count.sh, updated to count scheme code

--- count-code.sh
+++ count-code.sh
@@ -3,4 +3,4 @@
 # count lines of code for all ports
 #
 # note that .verb files are most similar to Forth, so using that to count
-cloc --include-ext=cpp,hpp,cs,py,lua,verb --force-lang="Forth",verb --force-lang="C++",hpp --exclude-dir=alphaexperiments,ATTIC,python .
+cloc --include-ext=cpp,hpp,cs,py,lua,verb,scm --force-lang="Forth",verb --force-lang="C++",hpp --exclude-dir=alphaexperiments,ATTIC,python .
