d572e9b64da40467c8bbe1864d2b1b19aa14829f
Frank McIngvale <fmcingvale@gmail.com>
2022-06-02 22:53:25 -0500

    bootstrap: make safer by compiling ALL .b files before replacing any of them ... in retrospect i can see this is what has been causing some crashes when replacing/renaming words in those files.

--- bootstrap/make-bootstrap.sh
+++ bootstrap/make-bootstrap.sh
@@ -8,6 +8,7 @@ PRODUCTION=1
 # the c++ repl is the default
 REPL='../c++/repl'
 
+# a sanity check in case this was run inadvertently ...
 if [ $PRODUCTION -eq 1 ]; then
 	echo "                     *** CAUTION ***"
 	echo ""
@@ -27,17 +28,28 @@ if [ "$line" == "YES" ];
 then
 	echo ""
 	declare -a Libraries=("boot" "init" "compiler")	
+	# do this in two steps:
+	#    (1) Compile all library files to temporary files
+	#    (2) After all are compiled, copy temp files over .b files
+	#
+	# this is needed especially in cases where there are interdependent changes
+	# between the files -- e.g. a function is moved from compiler.verb to init.verb --
+	# if the init.verb.b was replaced immediately, the compiler would then fail due to seeing
+	# a duplicate name between the new init.verb.b and the old compiler.verb.b file. 
+	# another example would be if a function name changed -- the old .b would still expect 
+	# the old name to exist and fail. this way is safer and keeps the .b files synced.
 	for name in ${Libraries[@]}; do
-		echo "Bootstrapping " "$name.verb"
-		
-		# trying to overwrite (at least) compiler.verb.b directly causes an error,
-		# so save to temp file then copy
-		#
+		echo "Compiling " "$name.verb"
+
 		# use -nocache to be safer in case i'm compiling changes to the caching code
 		# (PROBABLY this was only needed when the caching code was first added via patches.verb,
 		# to get around the chicken-and-egg problem of being unable to load patches.verb.b,
 		# but doesn't hurt to leave it in)
 		$REPL ../verb/compile.verb -nocache -- ../lib/$name.verb > ../lib/$name.verb.b.temp
+	done
+	# *after* all are compiled, copy the temp files over the .b files
+	for name in ${Libraries[@]}; do
+		echo "Installing " "$name.verb.b"
 		cp ../lib/$name.verb.b.temp ../lib/$name.verb.b
 		rm ../lib/$name.verb.b.temp
 	done
