2a7a4c49eacc90939c9de8e8a70a0da2a77f7408
Frank McIngvale <fmcingvale@gmail.com>
2022-07-31 20:09:06 -0500

    exclude reference/ dir from code count

--- count-code.sh
+++ count-code.sh
@@ -7,4 +7,4 @@ echo "*************************************************************"
 # count lines of code for all ports
 #
 # note that .verb files are most similar to Forth, so using that to count
-cloc --include-ext=cpp,hpp,cs,py,lua,verb,scm --force-lang="Forth",verb --force-lang="C++",hpp --exclude-dir=alphaexperiments,ATTIC,bootstrap .
+cloc --include-ext=cpp,hpp,cs,py,lua,verb,scm --force-lang="Forth",verb --force-lang="C++",hpp --exclude-dir=alphaexperiments,ATTIC,bootstrap,reference .
