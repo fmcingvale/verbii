c5f078a15dcb91d60b9177fc43ca5e1dc76a20d2
Frank McIngvale <fmcingvale@gmail.com>
2022-08-06 19:08:15 -0500

    added script to set VERBII_BOOT

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "*** Make sure you:"
+echo "***     Run this like: source setup.sh"
+echo "***     Run from root directory of the verbii source."
+
+# NOTE: The following code (finding the absolute pathname of this script)
+# is from https://www.baeldung.com/linux/bash-get-location-within-script
+
+SCRIPT_PATH="${BASH_SOURCE}"
+while [ -L "${SCRIPT_PATH}" ]; do
+  SCRIPT_DIR="$(cd -P "$(dirname "${SCRIPT_PATH}")" >/dev/null 2>&1 && pwd)"
+  SCRIPT_PATH="$(readlink "${SCRIPT_PATH}")"
+  [[ ${SCRIPT_PATH} != /* ]] && SCRIPT_PATH="${SCRIPT_DIR}/${SCRIPT_PATH}"
+done
+SCRIPT_PATH="$(readlink -f "${SCRIPT_PATH}")"
+SCRIPT_DIR="$(cd -P "$(dirname -- "${SCRIPT_PATH}")" >/dev/null 2>&1 && pwd)"
+
+echo "*** Setting VERBII_BOOT ..."
+export VERBII_BOOT=${SCRIPT_DIR}/lib
