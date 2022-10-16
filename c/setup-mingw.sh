ac48dd85ada0792445da62cee58fe75e54840d0e
Frank McIngvale <fmcingvale@gmail.com>
2022-10-16 08:28:55 -0500

    c: updated mingw setup with new gc options

--- c/setup-mingw.sh
+++ c/setup-mingw.sh
@@ -3,8 +3,17 @@
 # Setup build for mingw64
 rm -rf bin
 
-# NOTE: The MinSizeRel is so cmake will use -Os instead of -O3 -- -O3 causes
-# an immediate segfault under mingw currently (g++ 12.1.0)
-cmake -G "MSYS Makefiles" -DCMAKE_BUILD_TYPE=Release -S src -B bin
+# uncomment one of the following build types ...
 
+# to build WITHOUT garbage collection, set USE_NO_GC
+#
+# this can be useful when tracking down weird bugs to turn off all gc
+#cmake -G "MSYS Makefiles" -DCMAKE_BUILD_TYPE=Release -DUSE_NO_GC=1 -S src/ -B bin
+
+# to build WITH gc-object garbage collection
+#cmake -G "MSYS Makefiles" -DCMAKE_BUILD_TYPE=Release -DUSE_GC_OBJECT=1 -S src/ -B bin
+
+# build with Boehm GC (if found), else currently defaults to USE_NO_GC
+# (eventually need to change this to default to USE_GC_OBJECT once it is stable)
+cmake -G "MSYS Makefiles" -DCMAKE_BUILD_TYPE=Release -S src/ -B bin
 
