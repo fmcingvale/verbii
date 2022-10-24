971bc1ac15ca1c7867598e9b7f03a47b9d54ccad
Frank McIngvale <fmcingvale@gmail.com>
2022-10-24 15:46:56 -0500

    c: add option for Boehm GC for debugging build

--- c/src/build-debugging.sh
+++ c/src/build-debugging.sh
@@ -4,6 +4,9 @@
 #gcc -p -g -o main main.c deserialize.c errors.c interpreter.c langtypes.c native.c opcodes.c util.c gc_object.c xmalloc.c -lm
 
 # Build debugging version with gc-object
-gcc -p -g -DUSE_GC_OBJECT -o main main.c deserialize.c errors.c interpreter.c langtypes.c native.c opcodes.c util.c gc_object.c xmalloc.c -lm
+#gcc -p -g -DUSE_GC_OBJECT -o main main.c deserialize.c errors.c interpreter.c langtypes.c native.c opcodes.c util.c gc_object.c xmalloc.c -lm
+
+# Build with Boehm GC
+gcc -p -g -DUSE_BOEHM_GC -o main main.c deserialize.c errors.c interpreter.c langtypes.c native.c opcodes.c util.c gc_object.c xmalloc.c -lm -lgc
 
 cp main ..
